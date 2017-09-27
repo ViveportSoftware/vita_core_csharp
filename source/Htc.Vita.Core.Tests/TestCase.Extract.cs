bedd5dadbb906097bf2e152e686bf9c846b46d87
kenelin <kenelin@gmail.com>
2017-09-27 11:01:03 +0800

    Add class to extract .ico from PE binary

---
+++ source/Htc.Vita.Core.Tests/TestCase.Extract.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Htc.Vita.Core.Util;
+using Xunit;
+
+namespace Htc.Vita.Core.Tests
+{
+    public partial class TestCase
+    {
+        [Fact]
+        public void Extract_0_FromFileToIconInWindows()
+        {
+            var sourceFileInfo = new FileInfo("C:\\Windows\\System32\\shell32.dll");
+            Assert.True(sourceFileInfo.Exists);
+            var target = Environment.GetEnvironmentVariable("Temp");
+            Assert.NotNull(target);
+            target = Path.Combine(target, "Icon" + Util.Convert.ToTimestampInMilli(DateTime.UtcNow) + ".ico");
+            var targetFileInfo = new FileInfo(target);
+            Assert.True(Extract.FromFileToIcon(sourceFileInfo, targetFileInfo));
+            Assert.True(targetFileInfo.Exists);
+        }
+    }
+}
---
+++ source/Htc.Vita.Core/Util/Extract.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using Htc.Vita.Core.Log;
+using Htc.Vita.Core.Runtime;
+
+namespace Htc.Vita.Core.Util
+{
+    public class Extract
+    {
+        private static readonly Logger Log = Logger.GetInstance(typeof(Extract));
+
+        public static bool FromFileToIcon(FileInfo fromFile, FileInfo toIcon)
+        {
+            if (!Platform.IsWindows)
+            {
+                return false;
+            }
+            return FromFileToIconInWindows(fromFile, toIcon);
+        }
+
+        private static bool FromFileToIconInWindows(FileInfo fromFile, FileInfo toIcon)
+        {
+            if (fromFile == null || !fromFile.Exists || toIcon == null)
+            {
+                return false;
+            }
+
+            Icon icon = null;
+            var filePath = fromFile.FullName;
+            try
+            {
+                icon = Icon.ExtractAssociatedIcon(filePath);
+            }
+            catch (Exception e)
+            {
+                Log.Error("Can not extract icon from path \"" + filePath + "\"" + e.Message);
+            }
+            if (icon == null)
+            {
+                return false;
+            }
+
+            filePath = toIcon.FullName;
+            try
+            {
+                using (var stream = new FileStream(filePath, FileMode.CreateNew))
+                {
+                    icon.Save(stream);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.Error("Can not dump icon to path \"" + filePath + "\"" + e.Message);
+            }
+            return false;
+        }
+    }
+}
