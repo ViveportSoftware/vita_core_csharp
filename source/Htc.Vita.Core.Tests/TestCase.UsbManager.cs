dc5ee4c5a7b6f248685241e79c816a856863bbd5
kenelin <kenelin@gmail.com>
2017-10-17 12:34:28 +0800

    Update test cases

--- source/Htc.Vita.Core.Tests/TestCase.UsbManager.cs
+++ source/Htc.Vita.Core.Tests/TestCase.UsbManager.cs
@@ -18,8 +18,8 @@ namespace Htc.Vita.Core.Tests
             foreach (var deviceInfo in deviceInfos) {
                 Console.WriteLine("deviceInfo.Path: " + deviceInfo.Path);
                 Assert.False(string.IsNullOrWhiteSpace(deviceInfo.Path));
-                Assert.True(deviceInfo.ProductId.Length == 4);
-                Assert.True(deviceInfo.VendorId.Length == 4);
+                Assert.True(string.IsNullOrEmpty(deviceInfo.ProductId) || deviceInfo.ProductId.Length == 4);
+                Assert.True(string.IsNullOrEmpty(deviceInfo.VendorId) || deviceInfo.VendorId.Length == 4);
                 Assert.False(string.IsNullOrWhiteSpace(deviceInfo.Description));
                 Assert.False(string.IsNullOrWhiteSpace(deviceInfo.Manufecturer));
                 Assert.False(string.IsNullOrWhiteSpace(deviceInfo.Optional["type"]));
