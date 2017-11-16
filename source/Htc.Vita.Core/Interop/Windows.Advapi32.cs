a3ad41d53b754c5408480a9952fb67515ca21822
kenelin <kenelin@gmail.com>
2017-11-16 22:15:34 +0800

    Move Windows.Advapi32.OpenService.cs into Windows.Advapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
@@ -152,6 +152,20 @@ namespace Htc.Vita.Core.Interop
                     [In] string databaseName,
                     SCMAccessRight desiredAccess
             );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/ms684330.aspx
+             */
+            [DllImport(Libraries.Windows_advapi32,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            public static extern IntPtr OpenServiceW(
+                    IntPtr hSCManager,
+                    [In] string serviceName,
+                    ServiceAccessRight desiredAccess
+            );
         }
     }
 }
