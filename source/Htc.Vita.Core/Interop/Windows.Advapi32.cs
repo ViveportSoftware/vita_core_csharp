4ad7f812104300debc5aa6d4db62dc37efaa5149
kenelin <kenelin@gmail.com>
2017-11-16 22:21:26 +0800

    Move Windows.Advapi32.QueryServiceStatus.cs into Windows.Advapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
@@ -207,6 +207,20 @@ namespace Htc.Vita.Core.Interop
                     uint cbBufSize,
                     out uint pcbBytesNeeded
             );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/ms684939.aspx
+             */
+            [DllImport(Libraries.Windows_advapi32,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool QueryServiceStatus(
+                    [In] IntPtr hService,
+                    ref SERVICE_STATUS lpServiceStatus
+            );
         }
     }
 }
