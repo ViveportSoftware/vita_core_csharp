e2799fa88c95018a1c78e1dd0a5ee9d658276c9e
kenelin <kenelin@gmail.com>
2017-11-16 22:32:11 +0800

    Move Windows.Advapi32.ServiceAccessRight.cs into Windows.Advapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
@@ -80,6 +80,35 @@ namespace Htc.Vita.Core.Interop
             }
 
             /**
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/ms684330.aspx
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/ms685981.aspx
+             */
+            [Flags]
+            public enum ServiceAccessRight : uint
+            {
+                STANDARD_RIGHTS_REQUIRED = 0xF0000,
+                SERVICE_QUERY_CONFIG = 0x0001,
+                SERVICE_CHANGE_CONFIG = 0x0002,
+                SERVICE_QUERY_STATUS = 0x0004,
+                SERVICE_ENUMERATE_DEPENDENTS = 0x0008,
+                SERVICE_START = 0x0010,
+                SERVICE_STOP = 0x0020,
+                SERVICE_PAUSE_CONTINUE = 0x0040,
+                SERVICE_INTERROGATE = 0x0080,
+                SERVICE_USER_DEFINED_CONTROL = 0x0100,
+                SERVICE_ALL_ACCESS = STANDARD_RIGHTS_REQUIRED
+                                     | SERVICE_QUERY_CONFIG
+                                     | SERVICE_CHANGE_CONFIG
+                                     | SERVICE_QUERY_STATUS
+                                     | SERVICE_ENUMERATE_DEPENDENTS
+                                     | SERVICE_START
+                                     | SERVICE_STOP
+                                     | SERVICE_PAUSE_CONTINUE
+                                     | SERVICE_INTERROGATE
+                                     | SERVICE_USER_DEFINED_CONTROL
+            }
+
+            /**
              * https://msdn.microsoft.com/en-us/library/windows/desktop/ms684950.aspx
              */
             [StructLayout(LayoutKind.Sequential)]
