c0a60527f478a1b51d57c2df22a81dce36124dc5
kenelin <kenelin@gmail.com>
2017-11-16 21:57:36 +0800

    Move Windows.Advapi32.ConvertStringSidToSid.cs into Windows.Advapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
@@ -56,6 +56,20 @@ namespace Htc.Vita.Core.Interop
                     [In] IntPtr pSid,
                     [MarshalAs(UnmanagedType.LPTStr)] ref string sid
             );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/aa376402.aspx
+             */
+            [DllImport(Libraries.Windows_advapi32,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool ConvertStringSidToSidW(
+                    string sid,
+                    IntPtr pSid
+            );
         }
     }
 }
