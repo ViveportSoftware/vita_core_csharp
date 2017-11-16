abc470f25e828afd836feede8cc266d316223ce0
kenelin <kenelin@gmail.com>
2017-11-16 22:44:27 +0800

    Move Windows.Advapi32.StartType.cs into Windows.Advapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
@@ -162,6 +162,19 @@ namespace Htc.Vita.Core.Interop
             }
 
             /**
+             * https://msdn.microsoft.com/en-us/library/ms681987.aspx
+             */
+            public enum START_TYPE : uint
+            {
+                SERVICE_BOOT_START = 0x00000000,
+                SERVICE_SYSTEM_START = 0x00000001,
+                SERVICE_AUTO_START = 0x00000002,
+                SERVICE_DEMAND_START = 0x00000003,
+                SERVICE_DISABLED = 0x00000004,
+                SERVICE_NO_CHANGE = 0xffffffff
+            }
+
+            /**
              * https://msdn.microsoft.com/en-us/library/windows/desktop/ms684950.aspx
              */
             [StructLayout(LayoutKind.Sequential)]
