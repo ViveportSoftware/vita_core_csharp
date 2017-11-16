3dbc273f02f8191fcd4cea5012599bac532263c1
kenelin <kenelin@gmail.com>
2017-11-16 22:03:59 +0800

    Move Windows.Advapi32.CurrentState.cs into Windows.Advapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
@@ -30,6 +30,20 @@ namespace Htc.Vita.Core.Interop
             }
 
             /**
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/ms685996.aspx
+             */
+            public enum CURRENT_STATE : uint
+            {
+                SERVICE_STOPPED = 0x00000001,
+                SERVICE_START_PENDING = 0x00000002,
+                SERVICE_STOP_PENDING = 0x00000003,
+                SERVICE_RUNNING = 0x00000004,
+                SERVICE_CONTINUE_PENDING = 0x00000005,
+                SERVICE_PAUSE_PENDING = 0x00000006,
+                SERVICE_PAUSED = 0x00000007
+            }
+
+            /**
              * https://msdn.microsoft.com/en-us/library/ms681987.aspx
              */
             [DllImport(Libraries.Windows_advapi32,
