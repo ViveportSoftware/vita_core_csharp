5702cd9eee9d450e661a03bd5cf37d33d4efdcfe
kenelin <kenelin@gmail.com>
2017-11-16 22:09:49 +0800

    Move Windows.Advapi32.LookupAccountSid.cs into Windows.Advapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Htc.Vita.Core.Interop
 {
@@ -117,6 +118,25 @@ namespace Htc.Vita.Core.Interop
             public static extern bool ConvertStringSidToSidW(
                     string sid,
                     IntPtr pSid
+            );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/aa379166.aspx
+             */
+            [DllImport(Libraries.Windows_advapi32,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool LookupAccountSidW(
+                    [In] string pSystemName,
+                    IntPtr pSid,
+                    StringBuilder name,
+                    [MarshalAs(UnmanagedType.U4)] ref int cchName,
+                    StringBuilder referencedDomainName,
+                    [MarshalAs(UnmanagedType.U4)] ref int cchReferencedDomainName,
+                    out SID_NAME_USE peUse
             );
         }
     }
