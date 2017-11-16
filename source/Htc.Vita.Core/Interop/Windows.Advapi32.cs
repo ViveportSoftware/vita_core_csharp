6bcd1335b70f5fdefa909bf6de8c25bd20d192dd
kenelin <kenelin@gmail.com>
2017-11-16 22:06:25 +0800

    Move Windows.Advapi32.ErrorControlType.cs into Windows.Advapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Advapi32.cs
@@ -46,6 +46,18 @@ namespace Htc.Vita.Core.Interop
             /**
              * https://msdn.microsoft.com/en-us/library/ms681987.aspx
              */
+            public enum ERROR_CONTROL_TYPE : uint
+            {
+                SERVICE_ERROR_IGNORE = 0x00000000,
+                SERVICE_ERROR_NORMAL = 0x00000001,
+                SERVICE_ERROR_SEVERE = 0x00000002,
+                SERVICE_ERROR_CRITICAL = 0x00000003,
+                SERVICE_NO_CHANGE = 0xffffffff
+            }
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/ms681987.aspx
+             */
             [DllImport(Libraries.Windows_advapi32,
                     CallingConvention = CallingConvention.Winapi,
                     CharSet = CharSet.Unicode,
