e6cec900260e97740320374a0c97a86f6b04e0f5
kenelin <kenelin@gmail.com>
2017-11-17 12:35:16 +0800

    Move Windows.Kernel32.GetDiskFreeSpaceEx.cs into Windows.Kernel32.cs

--- source/Htc.Vita.Core/Interop/Windows.Kernel32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Kernel32.cs
@@ -125,6 +125,22 @@ namespace Htc.Vita.Core.Interop
             public static extern bool FreeLibrary(
                     [In] IntPtr hModule
             );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/aa364937.aspx
+             */
+            [DllImport(Libraries.Windows_kernel32,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool GetDiskFreeSpaceExW(
+                    [In] string directoryName,
+                    out ulong freeBytesAvailable,
+                    out ulong totalNumberOfBytes,
+                    out ulong totalNumberOfFreeBytes
+            );
         }
     }
 }
