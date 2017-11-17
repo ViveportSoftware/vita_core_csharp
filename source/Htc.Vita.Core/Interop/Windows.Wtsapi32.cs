81b2ef87b0b9cc439563823652aef98e7cba3545
kenelin <kenelin@gmail.com>
2017-11-17 14:28:53 +0800

    Move Windows.Wtsapi32.WTSEnumerateSessions.cs into Windows.Wtsapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
@@ -54,6 +54,23 @@ namespace Htc.Vita.Core.Interop
                     ref IntPtr ppProcessInfo,
                     [MarshalAs(UnmanagedType.U4)] ref int pCount
             );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/aa383833.aspx
+             */
+            [DllImport(Libraries.Windows_wtsapi32,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool WTSEnumerateSessionsW(
+                    [In] IntPtr hServer,
+                    [In] [MarshalAs(UnmanagedType.U4)] int reserved,
+                    [In] [MarshalAs(UnmanagedType.U4)] int version,
+                    ref IntPtr ppSessionInfo,
+                    [MarshalAs(UnmanagedType.U4)] ref int pCount
+            );
         }
     }
 }
