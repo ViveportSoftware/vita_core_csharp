107d9958ff4da4e4a4222247d8e56c2b0db1b6f5
kenelin <kenelin@gmail.com>
2017-11-17 11:53:21 +0800

    Move Windows.Kernel32.CreateFile.cs into Windows.Kernel32.cs

--- source/Htc.Vita.Core/Interop/Windows.Kernel32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Kernel32.cs
@@ -19,6 +19,24 @@ namespace Htc.Vita.Core.Interop
             public static extern bool CloseHandle(
                     [In] IntPtr hObject
             );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/aa363858.aspx
+             */
+            [DllImport(Libraries.Windows_kernel32,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            public static extern IntPtr CreateFileW(
+                    [In] string fileName,
+                    [In] [MarshalAs(UnmanagedType.U4)] GENERIC desiredAccess,
+                    [In] [MarshalAs(UnmanagedType.U4)] FILE_SHARE shareMode,
+                    IntPtr lpSecurityAttributes,
+                    [In] [MarshalAs(UnmanagedType.U4)] CREATION_DISPOSITION creationDisposition,
+                    [In] [MarshalAs(UnmanagedType.U4)] FILE_ATTRIBUTE_FLAG flagsAndAttributes,
+                    IntPtr hTemplateFile
+            );
         }
     }
 }
