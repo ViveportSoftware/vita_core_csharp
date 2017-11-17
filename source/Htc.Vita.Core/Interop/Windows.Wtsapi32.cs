454a52d1c3c85b2d8518bc051443b9e4277c8b9e
kenelin <kenelin@gmail.com>
2017-11-17 14:39:22 +0800

    Move Windows.Wtsapi32.WTSQuerySessionInformation.cs into Windows.Wtsapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
@@ -147,6 +147,23 @@ namespace Htc.Vita.Core.Interop
             public static extern IntPtr WTSOpenServerW(
                     [In] [MarshalAs(UnmanagedType.LPTStr)] string pServerName
             );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/aa383838.aspx
+             */
+            [DllImport(Libraries.Windows_wtsapi32,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool WTSQuerySessionInformationW(
+                    [In] IntPtr hServer,
+                    [In] int sessionId,
+                    [In] WTS_INFO_CLASS wtsInfoClass,
+                    out IntPtr ppBuffer,
+                    out uint pBytesReturned
+            );
         }
     }
 }
