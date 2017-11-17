8f4743c0b134bf2939344995673afedb9b73d43a
kenelin <kenelin@gmail.com>
2017-11-17 11:30:34 +0800

    Move Windows.Hid.HidD_GetSerialNumberString.cs into Windows.Hid.cs

--- source/Htc.Vita.Core/Interop/Windows.Hid.cs
+++ source/Htc.Vita.Core/Interop/Windows.Hid.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Htc.Vita.Core.Interop
 {
@@ -17,6 +18,21 @@ namespace Htc.Vita.Core.Interop
                     SetLastError = true)]
             public static extern void HidD_GetHidGuid(
                     out Guid guid
+            );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/windows/hardware/ff539683.aspx
+             */
+            [DllImport(Libraries.Windows_hid,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool HidD_GetSerialNumberString(
+                    IntPtr hidDeviceObject,
+                    [MarshalAs(UnmanagedType.LPWStr)] StringBuilder buffer,
+                    [MarshalAs(UnmanagedType.U4)] int bufferLength
             );
         }
     }
