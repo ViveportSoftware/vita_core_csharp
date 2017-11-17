2c0162af7e27271bea3fe5f01249237266c146d5
kenelin <kenelin@gmail.com>
2017-11-17 14:26:29 +0800

    Move Windows.Wtsapi32.WTSEnumerateProcesses.cs into Windows.Wtsapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
@@ -37,6 +37,23 @@ namespace Htc.Vita.Core.Interop
             public static extern void WTSCloseServer(
                     [In] IntPtr hServer
             );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/aa383831.aspx
+             */
+            [DllImport(Libraries.Windows_wtsapi32,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool WTSEnumerateProcessesW(
+                    [In] IntPtr hServer,
+                    [In] [MarshalAs(UnmanagedType.U4)] int reserved,
+                    [In] [MarshalAs(UnmanagedType.U4)] int version,
+                    ref IntPtr ppProcessInfo,
+                    [MarshalAs(UnmanagedType.U4)] ref int pCount
+            );
         }
     }
 }
