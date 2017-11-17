20d0523d2fa66cd0c15826f6aa4a98e3d7e7b613
kenelin <kenelin@gmail.com>
2017-11-17 13:30:49 +0800

    Move Windows.Wintrust.WTDProviderFlag.cs into Windows.Wintrust.cs

--- source/Htc.Vita.Core/Interop/Windows.Wintrust.cs
+++ source/Htc.Vita.Core/Interop/Windows.Wintrust.cs
@@ -29,6 +29,28 @@ namespace Htc.Vita.Core.Interop
             /**
              * https://msdn.microsoft.com/en-us/library/windows/desktop/aa388205.aspx
              */
+            [Flags]
+            public enum WTD_PROVIDERFLAG
+            {
+                WTD_USE_IE4_TRUST_FLAG = 1,
+                WTD_NO_IE4_CHAIN_FLAG = 2,
+                WTD_NO_POLICY_USAGE_FLAG = 4,
+                WTD_REVOCATION_CHECK_NONE = 16,
+                WTD_REVOCATION_CHECK_END_CERT = 32,
+                WTD_REVOCATION_CHECK_CHAIN = 64,
+                WTD_REVOCATION_CHECK_CHAIN_EXCLUDE_ROOT = 128,
+                WTD_SAFER_FLAG = 256,
+                WTD_HASH_ONLY_FLAG = 512,
+                WTD_USE_DEFAULT_OSVER_CHECK = 1024,
+                WTD_LIFETIME_SIGNING_FLAG = 2048,
+                WTD_CACHE_ONLY_URL_RETRIEVAL = 4096,
+                WTD_DISABLE_MD2_MD4 = 8192,
+                WTD_MOTW = 16384
+            }
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/aa388205.aspx
+             */
             [StructLayout(LayoutKind.Sequential)]
             public struct WINTRUST_DATA
             {
