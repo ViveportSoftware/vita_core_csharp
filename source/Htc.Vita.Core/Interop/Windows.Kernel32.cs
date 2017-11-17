78f5993ef365819dbaed700a9d16493cdfb2ef5c
kenelin <kenelin@gmail.com>
2017-11-17 12:18:36 +0800

    Move Windows.Kernel32.CreationDisposition.cs into Windows.Kernel32.cs

--- source/Htc.Vita.Core/Interop/Windows.Kernel32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Kernel32.cs
@@ -8,6 +8,18 @@ namespace Htc.Vita.Core.Interop
         public static partial class Kernel32
         {
             /**
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/aa363858.aspx
+             */
+            public enum CREATION_DISPOSITION
+            {
+                CREATE_NEW = 1,
+                CREATE_ALWAYS = 2,
+                OPEN_EXISTING = 3,
+                OPEN_ALWAYS = 4,
+                TRUNCATE_EXISTING = 5
+            }
+
+            /**
              * https://msdn.microsoft.com/en-us/library/windows/desktop/ms724211.aspx
              */
             [DllImport(Libraries.Windows_kernel32,
