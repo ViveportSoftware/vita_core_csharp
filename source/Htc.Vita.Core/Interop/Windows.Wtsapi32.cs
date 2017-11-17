c8fa4dc0106ea732738540e99ed2d4f7b2c98882
kenelin <kenelin@gmail.com>
2017-11-17 14:22:38 +0800

    Move Windows.Wtsapi32.WTSConnectStateClass.cs into Windows.Wtsapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
@@ -8,6 +8,23 @@ namespace Htc.Vita.Core.Interop
         public static partial class Wtsapi32
         {
             /**
+             * https://msdn.microsoft.com/en-us/library/aa383860.aspx
+             */
+            public enum WTS_CONNECTSTATE_CLASS
+            {
+                WTSActive,
+                WTSConnected,
+                WTSConnectQuery,
+                WTSShadow,
+                WTSDisconnected,
+                WTSIdle,
+                WTSListen,
+                WTSReset,
+                WTSDown,
+                WTSInit
+            }
+
+            /**
              * https://msdn.microsoft.com/en-us/library/aa383829.aspx
              */
             [DllImport(Libraries.Windows_wtsapi32,
