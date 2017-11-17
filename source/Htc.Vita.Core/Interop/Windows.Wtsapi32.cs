45bd0d3ad46ef76398837580566ce19b14af5be2
kenelin <kenelin@gmail.com>
2017-11-17 14:44:35 +0800

    Move Windows.Wtsapi32.WTSTerminateProcess.cs into Windows.Wtsapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
@@ -177,6 +177,21 @@ namespace Htc.Vita.Core.Interop
                     out IntPtr ppBuffer,
                     out uint pBytesReturned
             );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/aa383846.aspx
+             */
+            [DllImport(Libraries.Windows_wtsapi32,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool WTSTerminateProcess(
+                    [In] IntPtr hServer,
+                    [In] [MarshalAs(UnmanagedType.U4)] int processId,
+                    [In] int exitCode
+            );
         }
     }
 }
