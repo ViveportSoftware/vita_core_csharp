40ebb39f377960a3d2a9f744248bf557bd4a3f49
kenelin <kenelin@gmail.com>
2017-11-17 14:33:14 +0800

    Move Windows.Wtsapi32.WTSInfoClass.cs into Windows.Wtsapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
@@ -27,6 +27,43 @@ namespace Htc.Vita.Core.Interop
             }
 
             /**
+             * https://msdn.microsoft.com/en-us/library/aa383861.aspx
+             */
+            public enum WTS_INFO_CLASS
+            {
+                WTSInitialProgram,
+                WTSApplicationName,
+                WTSWorkingDirectory,
+                WTSOEMId,
+                WTSSessionId,
+                WTSUserName,
+                WTSWinStationName,
+                WTSDomainName,
+                WTSConnectState,
+                WTSClientBuildNumber,
+                WTSClientName,
+                WTSClientDirectory,
+                WTSClientProductId,
+                WTSClientHardwareId,
+                WTSClientAddress,
+                WTSClientDisplay,
+                WTSClientProtocolType,
+                WTSIdleTime,
+                WTSLogonTime,
+                WTSIncomingBytes,
+                WTSOutgoingBytes,
+                WTSIncomingFrames,
+                WTSOutgoingFrames,
+                WTSClientInfo,
+                WTSSessionInfo,
+                WTSSessionInfoEx,
+                WTSConfigInfo,
+                WTSValidationInfo,
+                WTSSessionAddressV4,
+                WTSIsRemoteSession
+            }
+
+            /**
              * https://msdn.microsoft.com/en-us/library/aa383829.aspx
              */
             [DllImport(Libraries.Windows_wtsapi32,
