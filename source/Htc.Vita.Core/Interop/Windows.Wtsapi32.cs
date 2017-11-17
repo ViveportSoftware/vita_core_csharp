18bc453c0793b64ea77410d81741a1c6c823c683
kenelin <kenelin@gmail.com>
2017-11-17 14:35:27 +0800

    Move Windows.Wtsapi32.WTSOpenServer.cs into Windows.Wtsapi32.cs

--- source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
+++ source/Htc.Vita.Core/Interop/Windows.Wtsapi32.cs
@@ -120,6 +120,18 @@ namespace Htc.Vita.Core.Interop
             public static extern void WTSFreeMemory(
                     [In] IntPtr pMemory
             );
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/aa383837.aspx
+             */
+            [DllImport(Libraries.Windows_wtsapi32,
+                    CallingConvention = CallingConvention.Winapi,
+                    CharSet = CharSet.Unicode,
+                    ExactSpelling = true,
+                    SetLastError = true)]
+            public static extern IntPtr WTSOpenServerW(
+                    [In] [MarshalAs(UnmanagedType.LPTStr)] string pServerName
+            );
         }
     }
 }
