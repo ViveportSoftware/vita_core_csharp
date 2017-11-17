f1356942cebc38c8a100f94f03f5406237900064
kenelin <kenelin@gmail.com>
2017-11-17 13:19:16 +0800

    Move Windows.Wintrust.WinTrustData.cs into Windows.Wintrust.cs

--- source/Htc.Vita.Core/Interop/Windows.Wintrust.cs
+++ source/Htc.Vita.Core/Interop/Windows.Wintrust.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Htc.Vita.Core.Interop
 {
@@ -12,6 +13,40 @@ namespace Htc.Vita.Core.Interop
             public static Guid WINTRUST_ACTION_GENERIC_CHAIN_VERIFY = new Guid("{FC451C16-AC75-11D1-B4B8-00C04FB66EA0}");
             public static Guid WINTRUST_ACTION_GENERIC_VERIFY_V2 = new Guid("{00AAC56B-CD44-11d0-8CC2-00C04FC295EE}");
             public static Guid WINTRUST_ACTION_TRUSTPROVIDER_TEST = new Guid("{573E31F8-DDBA-11d0-8CCB-00C04FC295EE}");
+
+            /**
+             * https://msdn.microsoft.com/en-us/library/windows/desktop/aa388205.aspx
+             */
+            [StructLayout(LayoutKind.Sequential)]
+            public struct WINTRUST_DATA
+            {
+                public uint cbStruct;
+
+                public IntPtr pPolicyCallbackData;
+
+                public IntPtr pSIPCallbackData;
+
+                public WTD_UI dwUIChoice;
+
+                public WTD_REVOKE fdwRevocationChecks;
+
+                public WTD_CHOICE dwUnionChoice;
+
+                public WTD_UNION_CHOICE infoUnion;
+
+                public WTD_STATEACTION dwStateAction;
+
+                public IntPtr hWVTStateData;
+
+                public IntPtr pwszURLReference;
+
+                public WTD_PROVIDERFLAG dwProvFlags;
+
+                public WTD_UICONTEXT dwUIContext;
+
+                // TODO
+                // WINTRUST_SIGNATURE_SETTINGS *pSignatureSettings;
+            }
         }
     }
 }
