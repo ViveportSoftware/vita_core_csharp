4afc0c492ba811e0ffc7359cae878d0ac5323a82
kenelin <kenelin@gmail.com>
2017-11-22 16:38:24 +0800

    Move TestCase.WebProxyFactory.cs to WebProxyFactory.cs

---
+++ source/Htc.Vita.Core.Tests/WebProxyFactory.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Htc.Vita.Core.Tests
+{
+    public static class WebProxyFactory
+    {
+        [Fact]
+        public static void Default_0_GetInstance()
+        {
+            var webProxyFactory = Net.WebProxyFactory.GetInstance();
+            Assert.NotNull(webProxyFactory);
+        }
+
+        [Fact]
+        public static void Default_1_GetWebProxy()
+        {
+            var webProxyFactory = Net.WebProxyFactory.GetInstance();
+            Assert.NotNull(webProxyFactory);
+            var webProxy = webProxyFactory.GetWebProxy();
+            Assert.NotNull(webProxy);
+        }
+    }
+}
