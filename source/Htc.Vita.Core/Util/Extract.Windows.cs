0bc951aff967047ef94a5331c5b85c5c701510a6
kenelin <kenelin@gmail.com>
2018-03-01 21:43:29 +0800

    Separate Extract.cs into multiple partial class files

---
+++ source/Htc.Vita.Core/Util/Extract.Windows.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Htc.Vita.Core.Util
+{
+    public static partial class Extract
+    {
+        internal static class Windows
+        {
+            internal static bool FromFileToIconInPlatform(FileInfo fromFile, FileInfo toIcon)
+            {
+                if (fromFile == null || !fromFile.Exists || toIcon == null)
+                {
+                    return false;
+                }
+
+                var targetPathDir = toIcon.Directory;
+                if (targetPathDir != null && !targetPathDir.Exists)
+                {
+                    targetPathDir.Create();
+                }
+
+                try
+                {
+                    var icon = Icon.ExtractAssociatedIcon(fromFile.FullName);
+                    if (icon == null)
+                    {
+                        return false;
+                    }
+                    using (var stream = new FileStream(toIcon.FullName, FileMode.CreateNew))
+                    {
+                        icon.Save(stream);
+                    }
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Log.Error("Can not extract icon to path: " + e.Message);
+                }
+                return false;
+            }
+        }
+    }
+}
--- source/Htc.Vita.Core/Util/Extract.cs
+++ source/Htc.Vita.Core/Util/Extract.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Drawing;
-using System.IO;
+﻿using System.IO;
 using Htc.Vita.Core.Log;
 using Htc.Vita.Core.Runtime;
 
 namespace Htc.Vita.Core.Util
 {
-    public static class Extract
+    public static partial class Extract
     {
         private static readonly Logger Log = Logger.GetInstance(typeof(Extract));
 
@@ -16,51 +14,7 @@ namespace Htc.Vita.Core.Util
             {
                 return false;
             }
-            return FromFileToIconInWindows(fromFile, toIcon);
-        }
-
-        private static bool FromFileToIconInWindows(FileInfo fromFile, FileInfo toIcon)
-        {
-            if (fromFile == null || !fromFile.Exists || toIcon == null)
-            {
-                return false;
-            }
-
-            var targetPathDir = toIcon.Directory;
-            if (!targetPathDir.Exists)
-            {
-                targetPathDir.Create();
-            }
-
-            Icon icon = null;
-            var filePath = fromFile.FullName;
-            try
-            {
-                icon = Icon.ExtractAssociatedIcon(filePath);
-            }
-            catch (Exception e)
-            {
-                Log.Error("Can not extract icon from path \"" + filePath + "\"" + e.Message);
-            }
-            if (icon == null)
-            {
-                return false;
-            }
-
-            filePath = toIcon.FullName;
-            try
-            {
-                using (var stream = new FileStream(filePath, FileMode.CreateNew))
-                {
-                    icon.Save(stream);
-                }
-                return true;
-            }
-            catch (Exception e)
-            {
-                Log.Error("Can not dump icon to path \"" + filePath + "\"" + e.Message);
-            }
-            return false;
+            return Windows.FromFileToIconInPlatform(fromFile, toIcon);
         }
     }
 }
