acd261083c90896eb3e02a4ad56620eb3c96022b
kenelin <kenelin@gmail.com>
2018-03-21 21:50:23 +0800

    Allow users group to access named pipe

--- source/Htc.Vita.Core/Runtime/NamedPipeIpcChannel.cs
+++ source/Htc.Vita.Core/Runtime/NamedPipeIpcChannel.cs
@@ -5,6 +5,8 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
 using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using System.Text;
 using System.Threading;
 using Htc.Vita.Core.Crypto;
@@ -160,13 +162,40 @@ namespace Htc.Vita.Core.Runtime
             private void OnHandleRequest(object data)
             {
                 var threadId = Thread.CurrentThread.ManagedThreadId;
+                var pipeSecurity = new PipeSecurity();
+                pipeSecurity.AddAccessRule(
+                        new PipeAccessRule(
+                                new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null),
+                                PipeAccessRights.ReadWrite | PipeAccessRights.CreateNewInstance,
+                                AccessControlType.Allow
+                        )
+                );
+                pipeSecurity.AddAccessRule(
+                        new PipeAccessRule(
+                                new SecurityIdentifier(WellKnownSidType.CreatorOwnerSid, null),
+                                PipeAccessRights.FullControl,
+                                AccessControlType.Allow
+                        )
+                );
+                pipeSecurity.AddAccessRule(
+                        new PipeAccessRule(
+                                new SecurityIdentifier(WellKnownSidType.LocalSystemSid, null),
+                                PipeAccessRights.FullControl,
+                                AccessControlType.Allow
+                        )
+                );
+
                 try
                 {
                     using (var serverStream = new NamedPipeServerStream(
                             _pipeName,
                             PipeDirection.InOut,
                             PipeThreadNumber,
-                            PipeTransmissionMode.Message
+                            PipeTransmissionMode.Message,
+                            PipeOptions.None,
+                            /* default */ 0,
+                            /* default */ 0,
+                            pipeSecurity
                     ))
                     {
                         while (!_shouldStopWorkers)
