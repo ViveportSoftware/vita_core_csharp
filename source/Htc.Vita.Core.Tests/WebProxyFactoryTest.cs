79244c33e225a56abd6dd7f2d3d82b08d699d180
kenelin <kenelin@gmail.com>
2019-03-28 12:33:23 +0800

    Rename WebProxyFactory to WebProxyFactoryTest

--- source/Htc.Vita.Core.Tests/WebProxyFactoryTest.cs
+++ source/Htc.Vita.Core.Tests/WebProxyFactoryTest.cs
@@ -1,13 +1,14 @@
-﻿using Xunit;
+﻿using Htc.Vita.Core.Net;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Htc.Vita.Core.Tests
 {
-    public class WebProxyFactory
+    public class WebProxyFactoryTest
     {
         private readonly ITestOutputHelper _output;
 
-        public WebProxyFactory(ITestOutputHelper output)
+        public WebProxyFactoryTest(ITestOutputHelper output)
         {
             _output = output;
         }
@@ -15,14 +16,14 @@ namespace Htc.Vita.Core.Tests
         [Fact]
         public static void Default_0_GetInstance()
         {
-            var webProxyFactory = Net.WebProxyFactory.GetInstance();
+            var webProxyFactory = WebProxyFactory.GetInstance();
             Assert.NotNull(webProxyFactory);
         }
 
         [Fact]
         public static void Default_1_GetWebProxy()
         {
-            var webProxyFactory = Net.WebProxyFactory.GetInstance();
+            var webProxyFactory = WebProxyFactory.GetInstance();
             Assert.NotNull(webProxyFactory);
             var webProxy = webProxyFactory.GetWebProxy();
             Assert.NotNull(webProxy);
@@ -31,12 +32,12 @@ namespace Htc.Vita.Core.Tests
         [Fact]
         public void Default_2_GetWebProxyStatus()
         {
-            var webProxyFactory = Net.WebProxyFactory.GetInstance();
+            var webProxyFactory = WebProxyFactory.GetInstance();
             Assert.NotNull(webProxyFactory);
             var webProxy = webProxyFactory.GetWebProxy();
             Assert.NotNull(webProxy);
             var webProxyStatus = webProxyFactory.GetWebProxyStatus(webProxy);
-            Assert.True(webProxyStatus != Net.WebProxyFactory.WebProxyStatus.Unknown);
+            Assert.True(webProxyStatus != WebProxyFactory.WebProxyStatus.Unknown);
             _output.WriteLine("WebProxyStatus: " + webProxyStatus);
         }
     }
