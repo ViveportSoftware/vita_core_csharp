69bdeea10c026ec59b3d36ad4e6d77cdbca0666a
kenelin <kenelin@gmail.com>
2019-04-16 20:19:23 +0800

    Add basic unit test for feature report

--- source/Htc.Vita.Core.Tests/UsbManagerTest.cs
+++ source/Htc.Vita.Core.Tests/UsbManagerTest.cs
@@ -46,5 +46,45 @@ namespace Htc.Vita.Core.Tests
                 }
             }
         }
+
+        [Fact]
+        public void Default_1_GetHidFeatureReport()
+        {
+            if (!Platform.IsWindows)
+            {
+                return;
+            }
+            var deviceInfos = UsbManager.GetHidDevices();
+            Assert.NotNull(deviceInfos);
+            foreach (var deviceInfo in deviceInfos)
+            {
+                _output.WriteLine("deviceInfo.Path: \"" + deviceInfo.Path + "\"");
+                _output.WriteLine("deviceInfo.SerialNumber: \"" + deviceInfo.SerialNumber + "\"");
+                Assert.False(string.IsNullOrWhiteSpace(deviceInfo.Path));
+                var productId = deviceInfo.ProductId;
+                if (!string.IsNullOrEmpty(productId))
+                {
+                    Assert.True(productId.Length == 4);
+                }
+                var vendorId = deviceInfo.VendorId;
+                if (!string.IsNullOrEmpty(vendorId))
+                {
+                    Assert.True(vendorId.Length == 4);
+                }
+                Assert.False(string.IsNullOrWhiteSpace(deviceInfo.Description));
+                Assert.False(string.IsNullOrWhiteSpace(deviceInfo.Manufacturer));
+                var optional = deviceInfo.Optional;
+                if (optional.ContainsKey("type"))
+                {
+                    Assert.False(string.IsNullOrWhiteSpace(optional["type"]));
+                }
+
+                var featureReport = UsbManager.GetHidFeatureReport(deviceInfo.Path, 0);
+                if (featureReport != null)
+                {
+                    _output.WriteLine("deviceInfo.FeatureReport: \"" + Util.Convert.ToHexString(featureReport) + "\"");
+                }
+            }
+        }
     }
 }
