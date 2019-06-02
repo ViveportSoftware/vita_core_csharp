3e3c53a97bb95bf85a370033847b58455579b4fa
kenelin <kenelin@gmail.com>
2019-06-03 01:56:59 +0800

    Add basic unit test for SecurityProtocolManager

---
+++ source/Htc.Vita.Core.Tests/SecurityProtocolManagerTest.cs
@@ -0,0 +1,24 @@
+﻿using Htc.Vita.Core.Net;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Htc.Vita.Core.Tests
+{
+    public class SecurityProtocolManagerTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public SecurityProtocolManagerTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Default_0_GetAvailableProtocol()
+        {
+            var availableProtocol = SecurityProtocolManager.GetAvailableProtocol();
+            Assert.NotEqual(0, (int) availableProtocol);
+            _output.WriteLine($@"availableProtocol: {availableProtocol}");
+        }
+    }
+}
