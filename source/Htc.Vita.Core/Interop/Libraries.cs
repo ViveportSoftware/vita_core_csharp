263a18ce22b1f627230654551fad2898c33cdec3
kenelin <kenelin@gmail.com>
2019-08-01 08:29:07 +0800

    Minor update

--- source/Htc.Vita.Core/Interop/Libraries.cs
+++ source/Htc.Vita.Core/Interop/Libraries.cs
@@ -3,16 +3,16 @@
     internal static class Libraries
     {
         internal const string WindowsAdvapi32 = "advapi32.dll";
-        internal const string WindowsCrypt32 = "crypt32.dll";
-        internal const string WindowsDnsapi = "dnsapi.dll";
-        internal const string WindowsDxgi = "dxgi.dll";
-        internal const string WindowsHid = "hid.dll";
+        internal const string WindowsCrypt32  =  "crypt32.dll";
+        internal const string WindowsDnsapi   =   "dnsapi.dll";
+        internal const string WindowsDxgi     =     "dxgi.dll";
+        internal const string WindowsHid      =      "hid.dll";
         internal const string WindowsKernel32 = "kernel32.dll";
-        internal const string WindowsPsapi = "psapi.dll";
+        internal const string WindowsPsapi    =    "psapi.dll";
         internal const string WindowsSetupapi = "setupapi.dll";
-        internal const string WindowsShell32 = "shell32.dll";
-        internal const string WindowsUser32 = "user32.dll";
-        internal const string WindowsUserenv = "userenv.dll";
+        internal const string WindowsShell32  =  "shell32.dll";
+        internal const string WindowsUser32   =   "user32.dll";
+        internal const string WindowsUserenv  =  "userenv.dll";
         internal const string WindowsWintrust = "wintrust.dll";
         internal const string WindowsWtsapi32 = "wtsapi32.dll";
     }
