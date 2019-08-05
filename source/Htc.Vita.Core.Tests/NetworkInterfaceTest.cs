2c1099d87ec3efcf72c8294f2fca94c3118c23b5
kenelin <kenelin@gmail.com>
2019-08-05 15:43:58 +0800

    Add IP address checking support in NetworkInterface

--- source/Htc.Vita.Core.Tests/NetworkInterfaceTest.cs
+++ source/Htc.Vita.Core.Tests/NetworkInterfaceTest.cs
@@ -1,4 +1,6 @@
-﻿using Htc.Vita.Core.Net;
+﻿using System.Net;
+using Htc.Vita.Core.Log;
+using Htc.Vita.Core.Net;
 using Xunit;
 
 namespace Htc.Vita.Core.Tests
@@ -12,9 +14,55 @@ namespace Htc.Vita.Core.Tests
         }
 
         [Fact]
-        public static void Default_0_IsInternetAvailable()
+        public static void Default_1_IsInternetAvailable()
         {
             Assert.True(NetworkInterface.IsInternetAvailable());
+        }
+
+        [Fact]
+        public static void Default_2_GetLocalIPv4AddressesWithSubnetMask()
+        {
+            var map = NetworkInterface.GetLocalIPv4AddressesWithSubnetMask();
+            Assert.NotEmpty(map);
+            foreach (var key in map.Keys)
+            {
+                Logger.GetInstance(typeof(NetworkInterfaceTest)).Info($"local address: {key}, mask: {map[key]}");
+            }
+        }
+
+        [Fact]
+        public static void Default_3_GetIPv4BroadcastAddress()
+        {
+            var address = IPAddress.Parse("127.0.0.1");
+            var subnetMask = IPAddress.Parse("255.0.0.0");
+            var broadcastAddress = NetworkInterface.GetIPv4BroadcastAddress(address, subnetMask);
+            Assert.Equal("127.255.255.255", broadcastAddress.ToString());
+            address = IPAddress.Parse("172.17.24.225");
+            subnetMask = IPAddress.Parse("255.255.255.240");
+            broadcastAddress = NetworkInterface.GetIPv4BroadcastAddress(address, subnetMask);
+            Assert.Equal("172.17.24.239", broadcastAddress.ToString());
+        }
+
+        [Fact]
+        public static void Default_4_IsInSameIPv4Subnet()
+        {
+            var firstAddress = IPAddress.Parse("192.168.56.1");
+            var secondAddress = IPAddress.Parse("192.168.56.234");
+            var subnetMask = IPAddress.Parse("255.255.255.0");
+            Assert.True(NetworkInterface.IsInSameIPv4Subnet(firstAddress, secondAddress, subnetMask));
+            secondAddress = IPAddress.Parse("192.168.99.1");
+            Assert.False(NetworkInterface.IsInSameIPv4Subnet(firstAddress, secondAddress, subnetMask));
+        }
+
+        [Fact]
+        public static void Default_5_GetLocalIPv4AddressesWithBroadcastAddress()
+        {
+            var map = NetworkInterface.GetLocalIPv4AddressesWithBroadcastAddress();
+            Assert.NotEmpty(map);
+            foreach (var key in map.Keys)
+            {
+                Logger.GetInstance(typeof(NetworkInterfaceTest)).Info($"local address: {key}, broadcast address: {map[key]}");
+            }
         }
     }
 }
--- source/Htc.Vita.Core/Net/NetworkInterface.cs
+++ source/Htc.Vita.Core/Net/NetworkInterface.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using Htc.Vita.Core.Log;
@@ -7,6 +10,131 @@ namespace Htc.Vita.Core.Net
 {
     public static class NetworkInterface
     {
+        public static IPAddress GetIPv4BroadcastAddress(
+                IPAddress address,
+                IPAddress subnetMask)
+        {
+            if (address == null || subnetMask == null)
+            {
+                return null;
+            }
+
+            var addressInBytes = address.GetAddressBytes();
+            var subnetMaskInBytes = subnetMask.GetAddressBytes();
+            if (addressInBytes.Length != subnetMaskInBytes.Length)
+            {
+                return null;
+            }
+
+            var broadcastAddressInBytes = new byte[addressInBytes.Length];
+            for (var i = 0; i < broadcastAddressInBytes.Length; i++)
+            {
+                broadcastAddressInBytes[i] = (byte)(addressInBytes[i] | (subnetMaskInBytes[i] ^ 255));
+            }
+            return new IPAddress(broadcastAddressInBytes);
+        }
+
+        public static Dictionary<IPAddress, IPAddress> GetLocalIPv4AddressesWithBroadcastAddress()
+        {
+            var result = new Dictionary<IPAddress, IPAddress>();
+            var addressesWithSubnetMask = GetLocalIPv4AddressesWithSubnetMask();
+            foreach (var address in addressesWithSubnetMask.Keys)
+            {
+                if (address == null)
+                {
+                    continue;
+                }
+
+                var subnetMask = addressesWithSubnetMask[address];
+                if (subnetMask == null)
+                {
+                    continue;
+                }
+
+                var broadcastAddress = GetIPv4BroadcastAddress(address, subnetMask);
+                if (broadcastAddress == null)
+                {
+                    continue;
+                }
+
+                result.Add(address, broadcastAddress);
+            }
+
+            return result;
+        }
+
+        public static Dictionary<IPAddress, IPAddress> GetLocalIPv4AddressesWithSubnetMask()
+        {
+            var result = new Dictionary<IPAddress, IPAddress>();
+            foreach (var networkInterface in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces())
+            {
+                var unicastAddresses = networkInterface?.GetIPProperties().UnicastAddresses;
+                if (unicastAddresses == null)
+                {
+                    continue;
+                }
+
+                foreach (var unicastAddress in unicastAddresses)
+                {
+                    if (unicastAddress == null)
+                    {
+                        continue;
+                    }
+
+                    if (unicastAddress.Address.AddressFamily != AddressFamily.InterNetwork)
+                    {
+                        continue;
+                    }
+
+                    result.Add(
+                            unicastAddress.Address,
+                            unicastAddress.IPv4Mask
+                    );
+                }
+            }
+
+            return result;
+        }
+
+        public static bool IsInSameIPv4Subnet(
+                IPAddress firstAddress,
+                IPAddress secondAddress,
+                IPAddress subnetMask)
+        {
+            var firstAddressInBytes = firstAddress.GetAddressBytes();
+            var secondAddressInBytes = secondAddress.GetAddressBytes();
+            var subnetMaskInBytes = subnetMask.GetAddressBytes();
+            if (firstAddressInBytes.Length != secondAddressInBytes.Length || firstAddressInBytes.Length != subnetMaskInBytes.Length)
+            {
+                return false;
+            }
+
+            var firstSubnetInBytes = new byte[firstAddressInBytes.Length];
+            var secondSubnetInBytes = new byte[secondAddressInBytes.Length];
+
+            var result = true;
+            for (var i = 0; i < subnetMaskInBytes.Length; i++)
+            {
+                firstSubnetInBytes[i] = (byte)(firstAddressInBytes[i] & subnetMaskInBytes[i]);
+                secondSubnetInBytes[i] = (byte)(secondAddressInBytes[i] & subnetMaskInBytes[i]);
+
+                if (firstSubnetInBytes[i] == secondSubnetInBytes[i])
+                {
+                    continue;
+                }
+
+                result = false;
+            }
+
+            var firstSubnet = new IPAddress(firstSubnetInBytes);
+            if (result)
+            {
+                Logger.GetInstance(typeof(NetworkInterface)).Debug($"{firstAddress} and {secondAddress} are in the same subnet: {firstSubnet}");
+            }
+
+            return result;
+        }
+
         public static bool IsNetworkAvailable()
         {
             return System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
