22fc3b7a212272b63a57feaab01338fe33a749a7
Kene Lin <kenelin@gmail.com>
2020-08-22 22:44:52 +0800

    Update documentation

--- source/Htc.Vita.Core/IO/UsbWatcherFactory.cs
+++ source/Htc.Vita.Core/IO/UsbWatcherFactory.cs
@@ -2,6 +2,9 @@ using Htc.Vita.Core.Util;
 
 namespace Htc.Vita.Core.IO
 {
+    /// <summary>
+    /// Class UsbWatcherFactory.
+    /// </summary>
     public abstract class UsbWatcherFactory
     {
         static UsbWatcherFactory()
@@ -9,28 +12,49 @@ namespace Htc.Vita.Core.IO
             TypeRegistry.RegisterDefault<UsbWatcherFactory, WmiUsbWatcherFactory>();
         }
 
+        /// <summary>
+        /// Registers the instance type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
         public static void Register<T>()
                 where T : UsbWatcherFactory, new()
         {
             TypeRegistry.Register<UsbWatcherFactory, T>();
         }
 
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <returns>UsbWatcherFactory.</returns>
         public static UsbWatcherFactory GetInstance()
         {
             return TypeRegistry.GetInstance<UsbWatcherFactory>();
         }
 
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>UsbWatcherFactory.</returns>
         public static UsbWatcherFactory GetInstance<T>()
                 where T : UsbWatcherFactory, new()
         {
             return TypeRegistry.GetInstance<UsbWatcherFactory, T>();
         }
 
+        /// <summary>
+        /// Creates the USB watcher.
+        /// </summary>
+        /// <returns>UsbWatcher.</returns>
         public UsbWatcher CreateUsbWatcher()
         {
             return OnCreateUsbWatcher();
         }
 
+        /// <summary>
+        /// Called when creating USB watcher.
+        /// </summary>
+        /// <returns>UsbWatcher.</returns>
         protected abstract UsbWatcher OnCreateUsbWatcher();
     }
 }
