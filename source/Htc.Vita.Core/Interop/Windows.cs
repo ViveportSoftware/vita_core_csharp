a20de609d24d652673ac1d1c6babfe185ed30312
Kene Lin <kenelin@gmail.com>
2020-08-29 17:32:51 +0800

    Implement IEquatable<DxgiAdapterDescription>

--- source/Htc.Vita.Core/Interop/Windows.cs
+++ source/Htc.Vita.Core/Interop/Windows.cs
@@ -1276,17 +1276,66 @@ namespace Htc.Vita.Core.Interop
         [ExternalReference("https://docs.microsoft.com/en-us/windows/win32/api/dxgi/ns-dxgi-dxgi_adapter_desc",
                 Description = "DXGI_ADAPTER_DESC structure")]
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-        internal struct DxgiAdapterDescription
+        internal struct DxgiAdapterDescription : IEquatable<DxgiAdapterDescription>
         {
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)] internal /* WCHAR[128] */ string description;
-            internal /* UINT       */ uint vendorId;
-            internal /* UINT       */ uint deviceId;
-            internal /* UINT       */ uint subSysId;
-            internal /* UINT       */ uint revision;
-            internal /* SIZE_T     */ UIntPtr dedicatedVideoMemory;
-            internal /* SIZE_T     */ UIntPtr dedicatedSystemMemory;
-            internal /* SIZE_T     */ UIntPtr sharedSystemMemory;
-            internal /* LUID       */ long adapterLuid;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)] internal readonly /* WCHAR[128] */ string description;
+                                                                  internal readonly /* UINT       */ uint vendorId;
+                                                                  internal readonly /* UINT       */ uint deviceId;
+                                                                  internal readonly /* UINT       */ uint subSysId;
+                                                                  internal readonly /* UINT       */ uint revision;
+                                                                  internal readonly /* SIZE_T     */ UIntPtr dedicatedVideoMemory;
+                                                                  internal readonly /* SIZE_T     */ UIntPtr dedicatedSystemMemory;
+                                                                  internal readonly /* SIZE_T     */ UIntPtr sharedSystemMemory;
+                                                                  internal readonly /* LUID       */ long adapterLuid;
+
+            public static bool operator ==(DxgiAdapterDescription left, DxgiAdapterDescription right)
+            {
+                return Equals(left, right);
+            }
+
+            public static bool operator !=(DxgiAdapterDescription left, DxgiAdapterDescription right)
+            {
+                return !Equals(left, right);
+            }
+
+            public bool Equals(DxgiAdapterDescription other)
+            {
+                return description == other.description
+                        && vendorId == other.vendorId
+                        && deviceId == other.deviceId
+                        && subSysId == other.subSysId
+                        && revision == other.revision
+                        && dedicatedVideoMemory.Equals(other.dedicatedVideoMemory)
+                        && dedicatedSystemMemory.Equals(other.dedicatedSystemMemory)
+                        && sharedSystemMemory.Equals(other.sharedSystemMemory)
+                        && adapterLuid == other.adapterLuid;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj))
+                {
+                    return false;
+                }
+                return obj is DxgiAdapterDescription && Equals((DxgiAdapterDescription) obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    var hashCode = (description != null ? description.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (int) vendorId;
+                    hashCode = (hashCode * 397) ^ (int) deviceId;
+                    hashCode = (hashCode * 397) ^ (int) subSysId;
+                    hashCode = (hashCode * 397) ^ (int) revision;
+                    hashCode = (hashCode * 397) ^ dedicatedVideoMemory.GetHashCode();
+                    hashCode = (hashCode * 397) ^ dedicatedSystemMemory.GetHashCode();
+                    hashCode = (hashCode * 397) ^ sharedSystemMemory.GetHashCode();
+                    hashCode = (hashCode * 397) ^ adapterLuid.GetHashCode();
+                    return hashCode;
+                }
+            }
         }
 
         [ExternalReference("https://docs.microsoft.com/en-us/windows/win32/api/dxgi/ns-dxgi-dxgi_output_desc",
