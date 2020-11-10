afcf26b9f0cb754e38902de4b8ab8143882572ff
Kene Lin <kenelin@gmail.com>
2020-11-10 16:37:46 +0800

    Update documentation

--- source/Htc.Vita.Core/IO/UsbManager.DeviceInfo.cs
+++ source/Htc.Vita.Core/IO/UsbManager.DeviceInfo.cs
@@ -4,15 +4,50 @@ namespace Htc.Vita.Core.IO
 {
     public static partial class UsbManager
     {
+        /// <summary>
+        /// Class DeviceInfo.
+        /// </summary>
         public class DeviceInfo
         {
+            /// <summary>
+            /// Gets or sets the description.
+            /// </summary>
+            /// <value>The description.</value>
             public string Description { get; set; } = "";
+            /// <summary>
+            /// Gets or sets the manufacturer name.
+            /// </summary>
+            /// <value>The manufacturer name.</value>
             public string Manufacturer { get; set; } = "";
+            /// <summary>
+            /// Gets or sets the path.
+            /// </summary>
+            /// <value>The path.</value>
             public string Path { get; set; } = "";
+            /// <summary>
+            /// Gets or sets the product name.
+            /// </summary>
+            /// <value>The product name.</value>
             public string Product { get; set; } = "";
+            /// <summary>
+            /// Gets or sets the product identifier.
+            /// </summary>
+            /// <value>The product identifier.</value>
             public string ProductId { get; set; } = "";
+            /// <summary>
+            /// Gets or sets the serial number.
+            /// </summary>
+            /// <value>The serial number.</value>
             public string SerialNumber { get; set; } = "";
+            /// <summary>
+            /// Gets or sets the vendor identifier.
+            /// </summary>
+            /// <value>The vendor identifier.</value>
             public string VendorId { get; set; } = "";
+            /// <summary>
+            /// Gets or sets the optional values.
+            /// </summary>
+            /// <value>The optional values.</value>
             public Dictionary<string, string> Optional { get; set; } = new Dictionary<string, string>();
         }
     }
