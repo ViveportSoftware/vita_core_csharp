da2b8ed2809402f29fa5b841ece3f8d53b7500ec
Kene Lin <kenelin@gmail.com>
2020-11-10 16:50:54 +0800

    Get rid of UTF-8 BOM

--- source/Htc.Vita.Core/Config/ConfigV2.cs
+++ source/Htc.Vita.Core/Config/ConfigV2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Htc.Vita.Core.Log;
 using Htc.Vita.Core.Util;
--- source/Htc.Vita.Core/Config/DummyConfigV2.cs
+++ source/Htc.Vita.Core/Config/DummyConfigV2.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Htc.Vita.Core.Log;
 
 namespace Htc.Vita.Core.Config
