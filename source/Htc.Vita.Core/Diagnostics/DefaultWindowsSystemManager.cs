fa5ef4a370f7c5a2150aeaebff0f86311f0e58ae
Kene Lin <kenelin@gmail.com>
2021-06-02 11:44:40 +0800

    Detect Windows 10 revision number

--- source/Htc.Vita.Core/Diagnostics/DefaultWindowsSystemManager.cs
+++ source/Htc.Vita.Core/Diagnostics/DefaultWindowsSystemManager.cs
@@ -54,7 +54,27 @@ namespace Htc.Vita.Core.Diagnostics
                 Logger.GetInstance(typeof(DefaultWindowsSystemManager)).Warn($"Version {result} detected. You may add app.manifest into your executable to correct Version API for Windows 8.1 or later");
             }
 
+            if (result.Major == 10
+                    && result.Minor == 0)
+            {
+                result = new Version(
+                        result.Major,
+                        result.Minor,
+                        result.Build,
+                        GetWindows10ProductRevisionFromRegistry()
+                );
+            }
+
             return result;
+        }
+
+        private static int GetWindows10ProductRevisionFromRegistry()
+        {
+            return Win32Registry.GetIntValue(
+                    Win32Registry.Hive.LocalMachine,
+                    "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion",
+                    "UBR"
+            );
         }
 
         /// <inheritdoc />
