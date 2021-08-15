0bbfef9b124a51cb5c2f219f7c36b322aee7e082
Kene Lin <kenelin@gmail.com>
2021-08-15 16:24:51 +0800

    Move FileTransferItem into FileTransfer.DataType.cs

--- source/Htc.Vita.Core/Net/FileTransfer.DataType.cs
+++ source/Htc.Vita.Core/Net/FileTransfer.DataType.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+
 namespace Htc.Vita.Core.Net
 {
     public partial class FileTransfer
@@ -12,6 +15,23 @@ namespace Htc.Vita.Core.Net
             /// </summary>
             /// <value>The error description.</value>
             public string ErrorDescription { get; set; }
+        }
+
+        /// <summary>
+        /// Class FileTransferItem.
+        /// </summary>
+        public class FileTransferItem
+        {
+            /// <summary>
+            /// Gets or sets the local path.
+            /// </summary>
+            /// <value>The local path.</value>
+            public FileInfo LocalPath { get; set; }
+            /// <summary>
+            /// Gets or sets the remote path.
+            /// </summary>
+            /// <value>The remote path.</value>
+            public Uri RemotePath { get; set; }
         }
     }
 }
