6e84c356e281881e3d07710568efaf5b795ee5fd
Kene Lin <kenelin@gmail.com>
2021-08-15 19:11:29 +0800

    Move DxgiOutput into Windows.ComWrapper.cs

--- source/Htc.Vita.Core/Interop/Windows.ComWrapper.cs
+++ source/Htc.Vita.Core/Interop/Windows.ComWrapper.cs
@@ -1542,5 +1542,78 @@ namespace Htc.Vita.Core.Interop
                 return new DxgiFactory(iDxgiFactory);
             }
         }
+
+        internal class DxgiOutput : IDisposable
+        {
+            private IDxgiOutput _dxgiOutput;
+            private bool _disposed;
+
+            internal DxgiOutput(IDxgiOutput dxgiOutput)
+            {
+                _dxgiOutput = dxgiOutput;
+            }
+
+            ~DxgiOutput()
+            {
+                Dispose(false);
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (_disposed)
+                {
+                    return;
+                }
+
+                if (disposing)
+                {
+                    // Disposing managed resource
+                }
+
+                if (_dxgiOutput == null)
+                {
+                    return;
+                }
+                if (Marshal.IsComObject(_dxgiOutput))
+                {
+#pragma warning disable CA1416
+                    Marshal.ReleaseComObject(_dxgiOutput);
+#pragma warning restore CA1416
+                }
+                _dxgiOutput = null;
+
+                _disposed = true;
+            }
+
+            internal DxgiOutputDescription GetDescription()
+            {
+                if (_dxgiOutput == null)
+                {
+                    return new DxgiOutputDescription();
+                }
+
+                try
+                {
+                    DxgiOutputDescription result;
+                    var dxgiError = _dxgiOutput.GetDesc(out result);
+                    if (dxgiError == DxgiError.SOk)
+                    {
+                        return result;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.GetInstance(typeof(DxgiOutput)).Error($"Can not get DXGI output description. error: {e.Message}");
+                }
+
+                return new DxgiOutputDescription();
+            }
+        }
     }
 }
