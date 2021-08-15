349964ca3010c80d6c59655b308808b41b118250
Kene Lin <kenelin@gmail.com>
2021-08-15 16:27:46 +0800

    Move FileTransferProgress into FileTransfer.DataType.cs

--- source/Htc.Vita.Core/Net/FileTransfer.DataType.cs
+++ source/Htc.Vita.Core/Net/FileTransfer.DataType.cs
@@ -378,5 +378,32 @@ namespace Htc.Vita.Core.Net
             /// <returns><c>true</c> if suspending this job successfully, <c>false</c> otherwise.</returns>
             protected abstract bool OnSuspend();
         }
+
+        /// <summary>
+        /// Class FileTransferProgress.
+        /// </summary>
+        public class FileTransferProgress
+        {
+            /// <summary>
+            /// Gets or sets the total bytes.
+            /// </summary>
+            /// <value>The total bytes.</value>
+            public ulong TotalBytes { get; set; }
+            /// <summary>
+            /// Gets or sets the total files.
+            /// </summary>
+            /// <value>The total files.</value>
+            public uint TotalFiles { get; set; }
+            /// <summary>
+            /// Gets or sets the transferred bytes.
+            /// </summary>
+            /// <value>The transferred bytes.</value>
+            public ulong TransferredBytes { get; set; }
+            /// <summary>
+            /// Gets or sets the transferred files.
+            /// </summary>
+            /// <value>The transferred files.</value>
+            public uint TransferredFiles { get; set; }
+        }
     }
 }
