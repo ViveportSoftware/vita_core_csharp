74162759ad78b85ffa48f1c8226188ae85c8755d
Kene Lin <kenelin@gmail.com>
2021-08-21 17:52:12 +0800

    Reduce warning, part 2

--- source/Htc.Vita.Core.Tests/HttpUtilityLiteTest.cs
+++ source/Htc.Vita.Core.Tests/HttpUtilityLiteTest.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace Htc.Vita.Core.Tests
 {
-    public class HttpUtilityLiteTest
+    public static class HttpUtilityLiteTest
     {
         [Fact]
         public static void Default_0_ParseQueryString()
