68944ca4b60f5555f03205c7550b18949262ae5e
Kene Lin <kenelin@gmail.com>
2021-08-22 14:29:21 +0800

    Move KeyStateFlag into Win32Registry.DataType.cs

--- source/Htc.Vita.Core/Util/Win32Registry.DataType.cs
+++ source/Htc.Vita.Core/Util/Win32Registry.DataType.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Htc.Vita.Core.Util
 {
     public static partial class Win32Registry
@@ -50,6 +52,13 @@ namespace Htc.Vita.Core.Util
             /// The permission to read write subtree
             /// </summary>
             ReadWriteSubTree = 2
+        }
+
+        [Flags]
+        internal enum KeyStateFlag
+        {
+            SystemKey   = 0x0001,
+            WriteAccess = 0x0002
         }
     }
 }
